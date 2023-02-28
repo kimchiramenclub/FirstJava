04578e09470d19ad19eadcd08a87a2230a78855f
kimchiramenclub <109577311+kimchiramenclub@users.noreply.github.com>
2023-02-28 16:53:55 +0900

    Update test.java

--- src/test.java
+++ src/test.java
@@ -3,6 +3,5 @@ public class test {
         public static void main(String[] args) {
             // TODO Auto-generated method stub
             System.out.println("Test!"); // 수정
-            System.out.println("TESTTEST");
         }
 }
